0611290d0c3a1c20d300d1f1fc6ddfaf2e09b504
LuisCasas <sc.luis@gmail.com>
2018-09-21 23:40:28 +0100

    Complete ajax call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
-import Axios from 'axios';
+import axios from 'axios';
 
 // to be changed to appropriate react export import
 const config = require('../../config/config');
 
 const API_KEY = config.api.API_KEY;
 
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
@@ -19,7 +19,3 @@ export function fetchWeather(city){
         payload: request
     };
 }
-
-/*
-    adding API request and Ajax with Axios
-*/
